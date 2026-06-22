a1a2510467fdd885ecaec93e73f5b65d301a0d3c
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2026-06-22 04:24:25 -0500

    Improve install_deps.sh output and silent mode

--- install_deps.sh
+++ install_deps.sh
@@ -3,11 +3,13 @@
 # installs the packages we use for the 'brb' script
 # assumes a debian-based system
 pkgs=("lolcat" "cowsay" "figlet")
+intro="~ ~ ~ ~ ~ brb installer ~ ~ ~ ~ ~"
 install_pkgs () {
-	sudo apt update && sudo apt install -y "${pkgs[@]}"
+	sudo apt update -qq && sudo apt install -y "${pkgs[@]}"
 }
 main () {
-	echo "installing packages needed for the 'brb' script..."; sleep 1
+	echo "$intro"; sleep 1
+	echo "Installing packages needed for the 'brb' script..."; sleep 1
 	install_pkgs
 	echo "Done."; sleep 1
 }
